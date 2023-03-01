fb7a49e4b0a8c32f11d7d1f8cb41a64f9635930a
Debajit Mallick <42708550+debajit13@users.noreply.github.com>
2023-03-01 18:25:24 +0530

    build the about page ui

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,31 @@
+import { Card, CardContent, Typography } from '@mui/material';
+import { Container } from '@mui/system';
+
 const About = () => {
-  return <h1>About Page</h1>;
+  return (
+    <Container>
+      <Card>
+        <CardContent>
+          <Typography
+            variant='h2'
+            component='h1'
+            align='center'
+            sx={{ marginBottom: 2 }}
+          >
+            About
+          </Typography>
+          <Typography variant='body1'>
+            Game Star is a one stop solution for all your game needs. When you
+            want to explore games we are not sure which game you want to play at
+            first. There Game Star helps you with its huge database of games. It
+            has game details with their type, platform specs, screenshot, genre
+            and a lot more. You can use this as a reference book to find your
+            next favourite game.
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 };
 
 export default About;
